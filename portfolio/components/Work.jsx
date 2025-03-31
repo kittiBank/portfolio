b4d153b6c8114be7288a5993f726eef88ae4525d
Kitti.Bank <kittitach.pro@gmail.com>
2025-03-31 22:22:21 +0700

    Add ContactPage, Edit picWorkpage

--- portfolio/components/Work.jsx
+++ portfolio/components/Work.jsx
@@ -12,28 +12,33 @@ const Work = () => {
                 Welcome to my web development portfolio! Explore a collection of projects showcasing.
             </p>
 
-            {/* <div className='grid grid-cols-auto my-10 gap-5'>
+            <div className='grid grid-cols-auto my-10 gap-5'>
                 {workData.map((project, index) => (
                     <div key={index}
-                        className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group:'
+                        className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
                         style={{ backgroundImage: `url(${project.bgImage})` }}>
                         <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 
-                        px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
+                        px-5 flex items-center justify-between duration-300 group-hover:bottom-10'>
                             <div>
-                                <h2>{project.title}</h2>
-                                <p>{project.description}</p>
+                                <h2 className='font-semibold'>{project.title}</h2>
+                                <p className='text-sm text-gray-700'>{project.description}</p>
                             </div>
-                            <div>
+                            <div className='border rounded-full border-black w-9 aspect-square flex items-center 
+                            justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-200 transition'>
                                 <Image src={assets.send_icon} alt='send icon' className='w-5' />
                             </div>
-
                         </div>
-
                     </div>
                 ))}
-            </div> */}
+            </div>
+
+            <a href="#" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px]
+            border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-gray-200 duration-300'>
+                Show more <Image src={assets.right_arrow_bold} alt='Right arrow' className='w-4' />
+            </a>
 
         </div>
+
     )
 }
 
